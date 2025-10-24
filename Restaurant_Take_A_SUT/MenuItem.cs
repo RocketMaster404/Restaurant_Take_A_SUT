5f539491bf40692e00df9452db1e420457325bfd
Pontus <Pontus_thoren@hotmail.com>
2025-10-24 09:05:13 +0200

    Added Class MenuItem

---
+++ Restaurant_Take_A_SUT/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Restaurant_Take_A_SUT
+{
+    internal class MenuItem
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public MenuItem(string name,int price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public MenuItem()
+        {
+            Name = "Item";
+            Price = 0;
+        }
+    }
+}
