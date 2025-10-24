68bcef5ef9b5c45708843264d85d096eb4836a46
Pontus <Pontus_thoren@hotmail.com>
2025-10-24 09:27:59 +0200

    Added the start ,menu and some properties

--- Restaurant_Take_A_SUT/Drinks.cs
+++ Restaurant_Take_A_SUT/Drinks.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Restaurant_Take_A_SUT
 {
-    internal class Drinks
+    internal class Drinks:MenuItem
     {
+        public string Brand { get; set; }
+        public string OriginRegion { get; set; }
+        public bool ContainAlc { get; set; }
+
+        public static List<Drinks> DrinkList = new List<Drinks>();
+        public Drinks(string name, int price, string brand, string originregion, bool containalc) : base(name, price)
+        {
+            Brand = brand;
+            OriginRegion = originregion;
+            ContainAlc = containalc;
+
+            DrinkList.Add(this);
+        }
+        public override string ToString()
+        {
+            string alcoholText = ContainAlc ? "Innehåller alkohol" : "Alkoholfri";
+            return $"{Name} ({Brand}) - {Price} kr, Från {OriginRegion}, {alcoholText}";
+        }
+        public static void ShowDrinkMenu()
+        {
+            Console.WriteLine($"\t======Dryckesmeny======");
+            foreach (var drink in DrinkList)
+            {
+                Console.WriteLine(drink);
+            }
+        }
     }
 }
